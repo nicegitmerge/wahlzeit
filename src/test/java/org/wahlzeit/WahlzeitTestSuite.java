fc23fd7f2fc55d887f81763b3e7eaab31faf63e3
a <b@c>
2017-11-11 09:41:01 +0100

    added test suite hierarchy

--- src/test/java/org/wahlzeit/WahlzeitTestSuite.java
+++ src/test/java/org/wahlzeit/WahlzeitTestSuite.java
@@ -3,43 +3,20 @@ package org.wahlzeit;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
-import org.wahlzeit.handlers.TellFriendTest;
-import org.wahlzeit.model.AccessRightsTest;
-import org.wahlzeit.model.CoordinateTest;
-import org.wahlzeit.model.FlagReasonTest;
-import org.wahlzeit.model.GenderTest;
-import org.wahlzeit.model.LocationTest;
-import org.wahlzeit.model.PhotoFilterTest;
-import org.wahlzeit.model.TagsTest;
-import org.wahlzeit.model.UserStatusTest;
-import org.wahlzeit.model.ValueTest;
-import org.wahlzeit.model.persistence.AbstractAdapterTest;
-import org.wahlzeit.model.persistence.DatastoreAdapterTest;
-import org.wahlzeit.services.EmailAddressTest;
-import org.wahlzeit.services.LogBuilderTest;
-import org.wahlzeit.services.mailing.EmailServiceTest;
-import org.wahlzeit.utils.StringUtilTest;
-import org.wahlzeit.utils.VersionTest;
+import org.wahlzeit.handlers.HandlersTestSuite;
+import org.wahlzeit.model.ModelTestSuite;
+import org.wahlzeit.model.persistence.ModelPersistenceTestSuite;
+import org.wahlzeit.services.ServicesTestSuite;
+import org.wahlzeit.services.mailing.MailingTestSuite;
+import org.wahlzeit.utils.UtilsTestSuite;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TellFriendTest.class,
-				AccessRightsTest.class,
-				CoordinateTest.class,
-				FlagReasonTest.class,
-				GenderTest.class,
-				LocationTest.class,
-				PhotoFilterTest.class,
-				TagsTest.class,
-				UserStatusTest.class,
-				ValueTest.class,
-				AbstractAdapterTest.class,
-				DatastoreAdapterTest.class,
-				EmailAddressTest.class,
-				LogBuilderTest.class,
-				EmailServiceTest.class,
-				StringUtilTest.class,
-				VersionTest.class})
+@SuiteClasses({ HandlersTestSuite.class,
+				ModelTestSuite.class,
+				ModelPersistenceTestSuite.class,
+				ServicesTestSuite.class,
+				MailingTestSuite.class,
+				UtilsTestSuite.class})
 public class WahlzeitTestSuite {
 
 }
